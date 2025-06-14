1dac3b4093145479758bd8d9b17c1f22327c5b8a
Jose-Prince <joseprince148@gmail.com>
2025-06-13 20:34:35 -0600

    Feat: set movement limit in y axis for players

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 function love.load()
+  screen_width = love.graphics.getWidth()
+  screen_height = love.graphics.getHeight()
   width = 40
   height = 200
 
@@ -11,16 +13,16 @@ end
 
 function love.update(dt)
   --Movement player 1
-  if love.keyboard.isDown("w") then
+  if love.keyboard.isDown("w") and y1 > 0 then
     y1 = y1 - 100 * dt
-  elseif love.keyboard.isDown("s") then
+  elseif love.keyboard.isDown("s") and y1 < screen_height - height then
     y1 = y1 + 100 * dt
   end
 
   --Movement player 2
-  if love.keyboard.isDown("up") then
+  if love.keyboard.isDown("up") and y2 > 0 then
     y2 = y2 - 100 * dt
-  elseif love.keyboard.isDown("down") then
+  elseif love.keyboard.isDown("down") and y2 < screen_height - height then
     y2 = y2 + 100 * dt
   end
 end
